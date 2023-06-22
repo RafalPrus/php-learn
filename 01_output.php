a55150db3d3b274a13b69fd6861a3ee7437162c2
Rafael <rafaelprues@gmail.com>
2023-06-22 20:04:10 +0200

    echo & print_r

---
+++ 01_output.php
@@ -0,0 +1,26 @@
+<?php
+// first one comment
+$first_arr = ['one', 'two', 'three'];
+echo 'First: ' . "<br>";
+print_r($first_arr);
+echo "<br>";
+echo 'Second: ' . "<br>";
+echo $first_arr[1];
+echo '<br>';
+var_dump($first_arr);
+echo "<br>";
+echo 'Hello World!!! :)';
+echo "\r\n";
+echo 123, 'Hello world';
+$new_line = '<br>';
+print_r($first_arr, true);
+    /*
+     * multiple
+     * lines
+     * comments
+     * test
+     */
+
+?>
+
+
