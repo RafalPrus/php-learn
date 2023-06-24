2e903e59cf0eaa2aa90acb9cdfbac68946e2ffa5
Rafael <rafaelprues@gmail.com>
2023-06-24 18:10:35 +0200

    conditionals 04_02

--- 04_conditionals.php
+++ 04_conditionals.php
@@ -44,6 +44,68 @@ if ($zero_str) {
 }
 // https://kunststube.net/isset/ more about isset and empty functions
 
+/*if ($empty_arr[0]) {
+    echo 'it is working';
+} else {
+    echo 'it is not working';
+}
+
+warning, udefined array*/
+
+echo '<br>';
+
+if (empty($empty_arr[0])) {
+    echo 'it is empty';
+} else {
+    echo 'it is not empty';
+}
+
+echo '<br>';
+$one = 'ABC';
+$two = 'abc';
+if ($one and !$two) {
+    echo 'one is true and two is false';
+} else {
+    echo 'not pass';
+}
+
+echo '<br>';
+
+if ($one or !$two) {
+    echo 'one is true or two is false';
+} else {
+    echo 'not pass';
+}
+
+//$one += $two; not working
+
+
+// working:
+$b = "Hello ";
+$b .= "World!";
+echo $b;
+
+echo '<br><br>';
+
+// ternary operator
+$foo = !$b ? 'is empty' : 'is not empty';
+echo $foo;
+
+if (!$b) {
+    echo 'is empty';
+} else {
+    echo 'is not empty';
+}
+
+echo '<br><br>';
+$action = empty($_POST['action']) ? 'default' : $_POST['action'];
+echo $action;
+
+echo '<br><br>';
+// echo (true ? 'true' : false ? 't' : 'f'); deprecated
+
+echo ((true ? 'true' : false) ? 't' : 'f');
+
 
 
 
