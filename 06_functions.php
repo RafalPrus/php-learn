26796b91ac82e88823f9946125e94b9da97bbbdf
Rafael <rafaelprues@gmail.com>
2023-06-28 17:51:28 +0200

    functions 06_02

--- 06_functions.php
+++ 06_functions.php
@@ -40,5 +40,58 @@ $user_name = 'Tom';
 welcomeUser($user_name);
 welcomeUser('Dominic');
 
+function sum($d, $e) {
+    return $d + $e;
+}
 
+$result = sum(10, 15);
+echo $result;
 
+function sumDefault($d, $e = 20) {
+    return $d + $e;
+}
+
+echo sumDefault(100);
+
+// anonymus function https://www.php.net/manual/en/functions.anonymous.php
+
+$substract = function ($var1, $var2) {
+    return $var1 - $var2;
+};
+echo $substract(100, 55);
+$num = 10;
+$num2 = 20;
+$expression = function ($var1, $var2) use ($num) {
+    global $num2;
+    $num2 = 111;
+    $num = 100;
+    return ($var1 - $var2) * $num;
+};
+echo '<br><br>';
+echo $expression(100, 99);
+echo '<br><br>';
+echo $num; // won't change value of $num in global scope
+echo '<br><br>';
+echo $num2; // global allows to change value globally in function
+// arrow function https://www.php.net/manual/en/functions.arrow.php
+
+$arr = ['car', 'bike', 'airplane'];
+
+function travelOptions($vehicles) {
+    foreach ($vehicles as $index => $vehicle) {
+        echo 'Option ' . $index + 1 . 'Vehicle type: ' . $vehicle . '<br>'; // generators? yield?
+    }
+}
+
+travelOptions($arr);
+
+function travelOptions2($vehicles) {
+    foreach ($vehicles as $index => $vehicle) {
+        yield 'Option ' . $index + 1 . 'Vehicle type: ' . $vehicle . '<br>';
+    }
+}
+// echo travelOptions2($arr); don't work, error
+
+foreach (travelOptions2($arr) as $value) {
+    echo $value;
+}
